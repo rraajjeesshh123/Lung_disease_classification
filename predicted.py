e02e7bd19af9269bba727d6bf158083140e98208
Rajesh kancherla <127394145+rraajjeesshh123@users.noreply.github.com>
2024-03-14 10:26:22 +0530

    Update predicted.py

--- predicted.py
+++ predicted.py
@@ -19,7 +19,7 @@ for filename in os.listdir(folder):
 
 X = np.asarray(photos, dtype='uint8')
 
-model = load_model(r"developed_model.h5")
+model = load_model("developed_model.h5")
 yhats2 = model.predict(X)
 
 max_predictions = (yhats2 == yhats2.max(axis=1, keepdims=1)).astype(int)
